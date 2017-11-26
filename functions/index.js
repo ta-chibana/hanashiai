7e25e28511103bb774b6c03614bb13bd1b81cc46
ta-chibana <tachibana.gmc@gmail.com>
2017-11-26 19:37:56 +0900

    Add push notifications function

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,36 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   response.send("Hello from Firebase!");
-// });
+exports.sendNotifications = functions.database.ref('/messages/{messageId}').onCreate(e => {
+  const snapshot = e.data;
+
+  const { message, userName } = snapshot.val();
+  const payload = {
+    notification: {
+      title: `${userName} posted message`,
+      body: message.length <= 100 ? message : `${message.substring(0, 97)}...`,
+      click_action: `https://${functions.config().firebase.authDomain}`
+    }
+  };
+
+  return admin.database().ref('fcmTokens').once('value').then(allTokens => {
+    if (allTokens.val()) {
+      const tokens = Object.keys(allTokens.val());
+
+      return admin.messaging().sendToDevice(tokens, payload).then(response => {
+        const tokensToRemove = [];
+        response.results.forEach((result, index) => {
+          const error = result.error;
+          if (error) {
+            console.error('Failure sending notification to', tokens[index], error);
+            if (error.code === 'messaging/invalid-registration-token' ||
+                error.code === 'messaging/registration-token-not-registered') {
+              tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
+            }
+          }
+        });
+        return Promise.all(tokensToRemove);
+      });
+    }
+  });
+});
